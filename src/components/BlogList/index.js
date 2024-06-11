0a798caba676d14071e5642e634e3d1df58fc4ed
teja6300 <218w1aa1239@vrsec.ac.in>
2024-06-11 06:26:53 +0000

    blog-app

---
+++ src/components/BlogList/index.js
@@ -0,0 +1,17 @@
+// Write your JS code here
+import './index.css'
+
+import BlogItem from '../BlogItem'
+
+const BlogList = props => {
+  const {blogsList} = props
+  return (
+    <ul className="blog-list-container">
+      {blogsList.map(eachBlog => (
+        <BlogItem key={eachBlog.id} blogDetails={eachBlog} />
+      ))}
+    </ul>
+  )
+}
+
+export default BlogList
---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,16 @@
+// Write your JS code here
+import './index.css'
+
+const UserInfo = () => {
+  ;<div className="user-info-container">
+    <img
+      className="profile-img"
+      src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+      alt="profile"
+    />
+    <h1 className="profile-name">Wade Warren</h1>
+    <p className="profile-description">Software Developer at UK</p>
+  </div>
+}
+
+export default UserInfo
